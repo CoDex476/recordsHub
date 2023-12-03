696b7d110e4e74651806999583017ba7cf079afa
CoDex476 <138341313+CoDex476@users.noreply.github.com>
2023-12-03 08:08:23 -0800

    ..

---
+++ records_hub/backend/myapp/serializers.py
@@ -0,0 +1,14 @@
+# myapp/serializers.py
+
+from rest_framework import serializers
+from .models import Memo, Receipt
+
+class MemoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Memo
+        fields = '__all__'
+
+class ReceiptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Receipt
+        fields = '__all__'
--- records_hub/backend/myapp/views.py
+++ records_hub/backend/myapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+# myapp/views.py
+from rest_framework import generics
+from .models import Memo, Receipt
+from .serializers import MemoSerializer, ReceiptSerializer
+
+class MemoListCreateView(generics.ListCreateAPIView):
+    queryset = Memo.objects.all()
+    serializer_class = MemoSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ReceiptListCreateView(generics.ListCreateAPIView):
+    queryset = Receipt.objects.all()
+    serializer_class = ReceiptSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
